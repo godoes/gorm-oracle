0b169a02e09cad70d25b5cee1330e9ed23a97f7c
liutianqi <zixizixi@vip.qq.com>
2022-11-21 14:33:34 +0800

    ⬆️ 更新三方模块依赖版本
    go: upgraded github.com/sijms/go-ora/v2 v2.5.32 => v2.5.33
    
    Signed-off-by: liutianqi <zixizixi@vip.qq.com>
    
    ⬆️ 更新三方模块依赖版本
    
    go: upgraded github.com/sijms/go-ora/v2 v2.5.31 => v2.5.32
    
    Signed-off-by: liutianqi <zixizixi@vip.qq.com>
    
    ⬆️ 更新三方模块依赖版本
    
    go: upgraded github.com/sijms/go-ora/v2 v2.5.30 => v2.5.31
    
    Signed-off-by: liutianqi <zixizixi@vip.qq.com>
    
    ⬆️ 更新三方模块依赖版本
    
    go: upgraded github.com/sijms/go-ora/v2 v2.5.29 => v2.5.30
    
    Signed-off-by: liutianqi <zixizixi@vip.qq.com>
    
    ⬆️ 更新三方模块依赖版本
    
    go: upgraded github.com/sijms/go-ora/v2 v2.5.27 => v2.5.29
    
    Signed-off-by: liutianqi <zixizixi@vip.qq.com>
    
    ⬆️ 更新三方模块依赖版本
    
    go: upgraded github.com/sijms/go-ora/v2 v2.5.26 => v2.5.27
    go: upgraded gorm.io/gorm v1.24.4 => v1.24.5
    
    Signed-off-by: liutianqi <zixizixi@vip.qq.com>
    
    ⬆️ 更新三方模块依赖版本
    
    go: upgraded github.com/sijms/go-ora/v2 v2.5.25 => v2.5.26
    go: upgraded gorm.io/gorm v1.24.3 => v1.24.4
    
    go: upgraded github.com/sijms/go-ora/v2 v2.5.23 => v2.5.25
    
    go: upgraded github.com/sijms/go-ora/v2 v2.5.22 => v2.5.23
    
    go: upgraded github.com/sijms/go-ora/v2 v2.5.21 => v2.5.22
    
    go: upgraded github.com/sijms/go-ora/v2 v2.5.20 => v2.5.21
    go: upgraded github.com/thoas/go-funk v0.9.2 => v0.9.3
    go: upgraded gorm.io/gorm v1.24.2 => v1.24.3
    
    go: upgraded github.com/sijms/go-ora/v2 v2.5.18 => v2.5.20
    
    go: upgraded github.com/sijms/go-ora/v2 v2.5.17 => v2.5.18
    
    go: upgraded github.com/sijms/go-ora/v2 v2.5.16 => v2.5.17
    
    go: upgraded github.com/sijms/go-ora/v2 v2.5.15 => v2.5.16
    
    go: upgraded github.com/sijms/go-ora/v2 v2.5.12 => v2.5.15
    
    go: upgraded github.com/sijms/go-ora/v2 v2.5.10 => v2.5.12
    
    go: upgraded github.com/sijms/go-ora/v2 v2.5.8 => v2.5.10
    
    go: upgraded github.com/sijms/go-ora/v2 v2.5.7 => v2.5.8
    
    go: upgraded github.com/sijms/go-ora/v2 v2.5.5 => v2.5.7
    go: upgraded gorm.io/gorm v1.24.1 => v1.24.2
    
    Signed-off-by: liutianqi <zixizixi@vip.qq.com>

--- _dev/script/go-mod.bat
+++ _dev/script/go-mod.bat
@@ -6,12 +6,20 @@ rem by iTanken
 
 cd /d %~dp0/../../
 echo 1. 更新三方依赖...
-cd & go get -d -u & echo.
+cd
+:: & go get -d -u & echo.
+go get -u github.com/emirpasic/gods
+go get -u github.com/sijms/go-ora/v2
+go get -u github.com/thoas/go-funk
+go get -u gorm.io/gorm
+echo.
 
 echo 2. 整理模块依赖...
 go mod tidy & echo.
 
 :: echo 3. 导入模块依赖到 vendor 目录...
 :: go mod vendor & echo.
+
+git add .
 
 call "%~dp0/done-time-pause.bat"

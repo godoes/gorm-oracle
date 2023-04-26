631883115ef932d0c2e6ba523dd75c10f11220f6
dzwvip <dzwvip@qq.com>
2023-04-26 12:23:17 +0800

    add oracle11 Limit
    (cherry picked from commit fd48c3ef5d94bef3a96d2925d5c9c971fe296a34)
    
    Close #7
    
    Signed-off-by: 子兮子兮 <zixizixi@vip.qq.com>

--- oracle.go
+++ oracle.go
@@ -1,8 +1,10 @@
 package oracle
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
+	"log"
 	"regexp"
 	"strconv"
 	"strings"
@@ -24,6 +26,7 @@ type Config struct {
 	DSN               string
 	Conn              gorm.ConnPool //*sql.DB
 	DefaultStringSize uint
+	DBVer             string
 }
 
 type Dialector struct {
@@ -79,7 +82,11 @@ func (d Dialector) Initialize(db *gorm.DB) (err error) {
 			return
 		}
 	}
-
+	err = db.ConnPool.QueryRowContext(context.Background(), "select version from product_component_version where rownum = 1").Scan(&d.DBVer)
+	if err != nil {
+		return err
+	}
+	//log.Println("DBver:" + d.DBVer)
 	if err = db.Callback().Create().Replace("gorm:create", Create); err != nil {
 		return
 	}
@@ -91,9 +98,18 @@ func (d Dialector) Initialize(db *gorm.DB) (err error) {
 }
 
 func (d Dialector) ClauseBuilders() map[string]clause.ClauseBuilder {
-	return map[string]clause.ClauseBuilder{
-		"LIMIT": d.RewriteLimit,
+	dbver, _ := strconv.Atoi(strings.Split(d.DBVer, ".")[0])
+	if dbver > 0 && dbver < 12 {
+		return map[string]clause.ClauseBuilder{
+			"LIMIT": d.RewriteLimit11,
+		}
+
+	} else {
+		return map[string]clause.ClauseBuilder{
+			"LIMIT": d.RewriteLimit,
+		}
 	}
+
 }
 
 func (d Dialector) RewriteLimit(c clause.Clause, builder clause.Builder) {
@@ -126,6 +142,36 @@ func (d Dialector) RewriteLimit(c clause.Clause, builder clause.Builder) {
 	}
 }
 
+// RewriteLimit11 Oracle11 Limit
+func (d Dialector) RewriteLimit11(c clause.Clause, builder clause.Builder) {
+	if limit, ok := c.Expression.(clause.Limit); ok {
+		if stmt, ok := builder.(*gorm.Statement); ok {
+			limitsql := strings.Builder{}
+			if limit := limit.Limit; *limit > 0 {
+				if _, ok := stmt.Clauses["WHERE"]; !ok {
+					limitsql.WriteString(" WHERE ")
+				} else {
+					limitsql.WriteString(" AND ")
+				}
+				limitsql.WriteString("ROWNUM <= ")
+				limitsql.WriteString(strconv.Itoa(*limit))
+			}
+			if _, ok := stmt.Clauses["ORDER BY"]; !ok {
+				_, _ = builder.WriteString(limitsql.String())
+			} else {
+				//  "ORDER BY" before  insert
+				sqltmp := strings.Builder{}
+				sqlold := stmt.SQL.String()
+				orderindx := strings.Index(sqlold, "ORDER BY") - 1
+				sqltmp.WriteString(sqlold[:orderindx])
+				sqltmp.WriteString(limitsql.String())
+				sqltmp.WriteString(sqlold[orderindx:])
+				log.Println(sqltmp.String())
+				stmt.SQL = sqltmp
+			}
+		}
+	}
+}
 func (d Dialector) DefaultValueOf(*schema.Field) clause.Expression {
 	return clause.Expr{SQL: "VALUES (DEFAULT)"}
 }
